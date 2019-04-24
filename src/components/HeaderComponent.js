689214999a4584c8bbb7ba10dbdece26779ea947
tanvi singh <tanvi.singh@lap0001796.yatraonline.local>
2019-04-24 14:43:44 +0530

    React router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,22 +1,54 @@
 import React, { Component } from 'react';
-import { Navbar , NavbarBrand ,Jumbotron } from 'reactstrap';
-
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false
+    };
+  }
+
+  toggleNav() {
+    this.setState (
+      {isNavOpen: !this.state.isNavOpen}
+    );
+  }
+
   render() {
     return (
         <React.Fragment>
-            <Navbar dark color="primary">
-            <div className="container">
-                <NavbarBrand href="/">Ristorante con fe</NavbarBrand>
-            </div>
+            <Navbar dark color="primary" expand="md">
+              <div className="container">
+                  <NavbarToggler onClick={this.toggleNav}/>
+                  <NavbarBrand className="mr-auto" href="/">
+                    <img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' />
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                  </NavbarBrand>
+              </div>
             </Navbar>
             <Jumbotron>
               <div className="container">
                 <div className="row row-header">
                   <div className="col-12 col-sm-6">
                     <h1>Ristorante con fusion</h1>
-                    <p>some textv trivial unimp unimp unimp some textv trivial unimp unimp unimp some textv trivial unimp unimp unimp some textv trivial unimp unimp unimp</p>
+                    <p>some textv triviaßl unimp unimp unimp some textv trivial unimp unimp unimp some textv trivial unimp unimp unimp some textv trivial unimp unimp unimp</p>
                   </div>
                 </div>
               </div>   
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,8 @@ import {DISHES} from '../shared/dishes';
 import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -23,20 +25,20 @@ class Main extends Component {
     console.log('inshow:',this.state);
   }
 
-
-
-
-
-
-
-
   render() {
     return (
       <div>
+
         <Header/>
+        <Switch>
+          <Route path='/home' component={Home} />
+          <Route exact path='/menu' component ={ ()=> <Menu dishes={this.state.dishes}/>} />
+          <Redirect to='/home' />
+        </Switch>
           <Menu dishes={this.state.dishes} onClick={(dish) => {console.log('dishID',dish);return this.showImg(dish)}}/>
           {this.state.selectedDish && (<DishDetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)}/>)}
         <Footer/>
+
       </div>
     );
   }
